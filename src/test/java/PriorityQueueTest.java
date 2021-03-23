330bd073a74a043248198031f519c3b6562e0b4d
p870613 <p870613@yahoo.com.tw>
2021-03-23 13:59:57 +0800

    test case

---
+++ src/test/java/PriorityQueueTest.java
@@ -0,0 +1,76 @@
+import java.util.PriorityQueue;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class PriorityQueueTest {
+    static Stream<Arguments> testcase() {
+        return Stream.of(
+                arguments(new int[]{3, 10, -1, -22}, new int[]{-22, -1, 3, 10}),
+                arguments(new int[]{-6, 1, 100, 5}, new int[]{-6, 1, 5, 100}),
+                arguments(new int[]{-41, -1000, -9, -6, -10}, new int[]{-1000, -41, -10, -9, -6}),
+                arguments(new int[]{-3, 1, 11, 0, 9, 3}, new int[]{-3, 0, 1, 3, 9, 11}),
+                arguments(new int[]{100, 10 ,20 ,-1 ,-3}, new int[]{-3, -1, 10, 20, 100})
+        );
+    }
+
+    @ParameterizedTest(name="#{index} - Test with Argument={0},{1}")
+    @MethodSource("testcase")
+    public void PriorityQueue_RunTest(int[] random_array,int[] correct_array){
+
+        PriorityQueue<Integer> t = new PriorityQueue<Integer>();
+        Integer s;
+        int result[] = new int[random_array.length];
+
+        for(int i = 0; i < random_array.length; i++){
+            t.offer(random_array[i]);
+        }
+
+        for(int i = 0; i < random_array.length; i++){
+            result[i] = t.poll();
+        }
+
+        assertArrayEquals(correct_array,result);
+    }
+
+    @Test
+    public void whenExceptionThrown_IllegalArgumentException(){
+        assertThrows(IllegalArgumentException.class,()->{
+            PriorityQueue<Integer> priorityqueue=new PriorityQueue<Integer>(-1,null);
+        });
+    }
+    @Test
+    public void whenExceptionThrown_NullPointerException(){
+        assertThrows(NullPointerException.class,()->{
+            PriorityQueue<Integer>priorityqueue=new PriorityQueue<Integer>();
+            priorityqueue.offer(null);
+        });
+    }
+    @Test
+    public void whenExceptionThrown_NoSuchElementException(){
+        assertThrows(NoSuchElementException.class,()->{
+            PriorityQueue<Integer>priorityqueue=new PriorityQueue<Integer>();
+            priorityqueue.remove();
+        });
+    }
+//    @Test
+//    public void whenExceptionThrown_ClassNotFoundException(){
+//        assertThrows(ClassNotFoundException.class,()->{
+//            PriorityQueue<Integer>priorityqueue=new PriorityQueue<Integer>();
+//
+//        });
+//    }
+//    @Test
+//    public void whenExceptionThrown_thenIndexOutOfBoundsException(){
+//        assertThrows(IndexOutOfBoundsException.class,()->{
+//            PriorityQueue<Integer>priorityqueue=new PriorityQueue<Integer>();
+//            priorityqueue.size();
+//        });
+//    }
+
+}
